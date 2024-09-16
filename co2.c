07bbc6fbec6da41f84281cf5ba9c57badb18e099
wenbo.zhang <1992552138@qq.com>
2024-09-17 03:10:45 +0800

    1、C语言学习到函数的作用域； 2、章节为ARM使用预处理指令#define；

---
+++ co2.c
@@ -0,0 +1,20 @@
+/**
+ * *****************************************************************************
+ * @file        co2.c
+ * @brief       
+ * @author      wenbo.zhang (1992552138@qq.com)
+ * @date        2024-09-17
+ * @copyright   xxxx技有限公司
+ * *****************************************************************************
+ */
+
+#include "co2.h"
+/**
+ * @brief       Get the Raw Tem object
+ * 
+ * @return      int32_t 
+ */
+uint32_t GetRawDataCo2(void)
+{
+    return 200;
+}
